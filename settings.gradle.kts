99a672d9f3392d0e75722c70856c203051f49592
Palpha01 <113171530+Palpha01@users.noreply.github.com>
2024-04-20 19:07:57 +0400

    The app called Tactics Sim for Assessment 2

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AAS - Assessment 2 pt.1 (Calculator)"
+include(":app")
+ 
